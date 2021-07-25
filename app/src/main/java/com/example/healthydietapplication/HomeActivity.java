d50e381b64eafd7c41d9477e4ea34df8942ebd3a
Viktorija Vrzhovska <viktorija-vrzovska@hotmail.com>
2021-07-26 01:43:08 +0200

    second commit

---
+++ app/src/main/java/com/example/healthydietapplication/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.healthydietapplication;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+}
---
+++ app/src/main/java/com/example/healthydietapplication/MyProfileActivity.java
@@ -0,0 +1,2 @@
+package com.example.healthydietapplication;public class MyProfileActivity {
+}
